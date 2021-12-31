1412500089bc112ab4408b2c4cbcbf491d476e7d
aaldosari1 <60715041+aaldosari1@users.noreply.github.com>
2021-12-31 20:43:43 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import logo from './logo.svg';
-//import List from "./components/List";
 import './App.css';
 
 function App() {
   
   const numbers = ["Walking", "coding", "laughing"];
-    //const listItems = numbers.map((number) =>
-  
 
   return (
-    
-    
     <div className="App">
-     
-    
      <a href={ "https://tuwaiq.edu.sa/" }>  <button> click  </button> </a>
      
      <h2>It is {new Date().toLocaleTimeString()}.</h2>
@@ -23,8 +16,6 @@ function App() {
      <li>{numbers[2]}</li>
     
     </div>
-
-    
   );
 }
 
